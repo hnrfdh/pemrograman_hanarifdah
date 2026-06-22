121ed3fc2fb7c12cc12b0a0fc2dc3f1f90b58edb
hnrfdh <hanarianra@gmail.com>
2026-06-22 22:44:00 +0700

    init

---
+++ src/main/java/view/sekolah/utshana/UtsHana.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package view.sekolah.utshana;
+
+/**
+ *
+ * @author devel
+ */
+public class UtsHana {
+
+    public static void main(String[] args) {
+        FormUtama view = new FormUtama();
+        view.setLocationRelativeTo(null);
+        view.setVisible(true);
+    }
+}
